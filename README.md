11e64e4fa2c0c1393994d7436b2027110b9f25de
kiwi1005 <53566106+kiwi1005@users.noreply.github.com>
2022-06-12 23:05:45 +0800

    Add files via upload
    main

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
-# STAS_SEG_Pytorch
+# readme
+
+
+# 環境建置
+	>使用conda 安裝 py36.yaml 以及 labelme.yaml
+	ex : conda env create -f environment.yaml
+		>若無法執行 請試著重新安裝或者清除快取及其他有可能干擾之軟體
+		若真的不行 再嘗試使用 : conda create --name py36 python=3.6
+								conda install -c conda-forge nb_conda
+								pip install torch==1.10.1+cu113 torchvision==0.11.2+cu113 torchaudio==0.10.1+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html
+								conda install git
+								pip install git+https://github.com/albumentations-team/albumentations
+								pip install git+https://github.com/qubvel/segmentation_models.pytorch
+								
+								
+	
+
+
+# 資料前處理
+	label to mask.ipynb 內有詳細步驟
+
+# 程式檔案
+	## 訓練
+	> train.py 執行的主程式
+		>執行方法: python train.py 
+			>詳細參數可參考主程式內 def get_args() 副函式 
+			ex:python train.py --amp -s 1 -l 1e-5 -f checkpoint.pth -b 2
+			-s:scale 縮放倍率, --amp:使用nvidia mix precition 加速,-b:batch_size ,-f:讀取之前訓練的結果 ,-l:設定學習率
+	### 資料結構
+	├── train.py
+	│
+	├── data
+	│   ├── imgs
+	│	│	│── *.jpg
+	│	│	│	...
+	│	│   └── *.jpg
+	│   ├── masks
+	│	│	│── *.png
+	│	│	│	...
+	│	│   └── *.png
+	│   └── 
+	├── p
+	│	│── predict.py
+	│	│── utils
+	│	│	│── data_loading.py
+			│── ...
+			└── 
+
+	## 預測
+	> predict.py 輸出預測結果的主程式 
+		先使用Predict Preprocess.ipynb 程式產生預測輸入與輸出的對應檔名的txt文字檔,程式將會讀取文字檔作為參數傳入執行預測。
+		ex:python predict.py
+	>Predict Preprocess.ipynb
+		使用jupyter notebook 等程式開啟 並cd至predict.py 路徑下
+			model: 要輸入的模型的檔案位置
+			input_folder:要預測的圖片的資料夾路徑
+			output_folder:輸出預測mask位置
+		執行完會在根目錄產生一個predictALL.txt 即完成。
+	>調整預測參數的方式
+		請進到./P/util/data_loading.py內 126 行的位置修改，最後一次驗證時有使用A.FancyPCA。
+
+# 訓練過程
+	
+	## BEST MODEL
+	1. 訓練過程
+		> 100 epoch lr=1e-5 @17 > 50 epoch lr=1e-5 @18 > 50 epoch lr=8e-6 @19 > 80 epoch lr=4e-6 @21 > 80 lr=4e-6 from 46 @26 | END
+		除了最後一段訓練，也就是@26 換了不同的隨機種子 其餘種子皆相同。
+		並且最後一次訓練將所有的圖像增強都取消只留corp
+
---
+++ utils/data_loading.py
@@ -0,0 +1,194 @@
+import logging
+from os import listdir
+from os.path import splitext
+from pathlib import Path
+import torchvision
+from torchvision import transforms
+from PIL import Image
+import numpy as np
+import torch
+from PIL import Image
+from torch.utils.data import Dataset
+import albumentations as A
+
+
+ 
+
+
+class BasicDataset(Dataset):
+
+    
+    
+    def __init__(self, images_dir: str, masks_dir: str, scale: float = 1.0, mask_suffix: str = ''):
+        self.images_dir = Path(images_dir)
+        self.masks_dir = Path(masks_dir)
+        assert 0 < scale <= 1, 'Scale must be between 0 and 1'
+        self.scale = scale
+        self.mask_suffix = mask_suffix
+
+        self.ids = [splitext(file)[0] for file in listdir(images_dir) if not file.startswith('.')]
+        if not self.ids:
+            raise RuntimeError(f'No input file found in {images_dir}, make sure you put your images there')
+        logging.info(f'Creating dataset with {len(self.ids)} examples')
+
+    def __len__(self):
+        return len(self.ids)
+    
+    @staticmethod
+    def preprocess(pil_img, scale, is_mask):
+        w, h = pil_img.size
+        
+        newW, newH = int(scale * w), int(scale * h)
+        # newW, newH = int(1700), int(928)## for predict!!
+        assert newW > 0 and newH > 0, 'Scale is too small, resized images would have no pixel'
+        pil_img = pil_img.resize((newW, newH), resample=Image.NEAREST if is_mask else Image.BICUBIC)
+        
+        #變成PIL
+        
+        
+        
+        # print("A:",type(pil_img))
+        transform1 = transforms.Compose([
+            # transforms.RandomAdjustSharpness(1.2, p=0.5),
+            # transforms.RandomRotation(30, expand=False, fill=255, resample=None),
+            transforms.CenterCrop((600,600)),
+            
+            transforms.ToTensor(),
+            # transforms.Normalize((0.8263105, 0.6212347, 0.7698729), (0.15582965, 0.268009, 0.1718304)),
+            transforms.ToPILImage(),
+            ])
+        # scripted_transforms = torch.jit.script(transform)
+        transform2 = transforms.Compose([
+            transforms.CenterCrop((600,600)),
+            ])
+        
+        # if not is_mask:
+            # pil_img = transform1(pil_img)
+        
+        # if is_mask:
+            # pil_img = transform2(pil_img)
+        
+        
+        
+        # pil_img = transform1(pil_img)
+        # print("B",type(pil_img))
+        pil_img = np.asarray(pil_img) 
+        # print("C",type(pil_img))
+        # pil_img = transforms.ToPILImage(pil_img)
+        img_ndarray = np.asarray(pil_img)
+        # print("DDD",type(img_ndarray))
+        
+        
+        
+        
+
+        if not is_mask:
+            if img_ndarray.ndim == 2:
+                img_ndarray = img_ndarray[np.newaxis, ...]
+            else:
+                img_ndarray = img_ndarray.transpose((2, 0, 1))
+
+            img_ndarray = img_ndarray / 255
+
+        return img_ndarray
+
+    @staticmethod
+    def load(filename):
+        ext = splitext(filename)[1]
+        if ext in ['.npz', '.npy']:
+            return Image.fromarray(np.load(filename))
+        elif ext in ['.pt', '.pth']:
+            return Image.fromarray(torch.load(filename).numpy())
+        else:
+            return Image.open(filename)
+    @staticmethod
+    def dataAug(img,mask):
+        transform = A.Compose([
+            
+            # A.RandomSizedCrop(min_max_height=(512-100, 512+100), height=512, width=512, p=1),
+            A.HorizontalFlip(p=0.5),
+            A.RandomBrightnessContrast(p=0.2),
+	    A.CLAHE(clip_limit=2.0, tile_grid_size=(16, 16), always_apply=False, p=0.8),
+	    
+            A.GaussianBlur(blur_limit=(1, 3), sigma_limit=0, always_apply=False, p=0.5),
+	    A.Sharpen(alpha=(0.3, 0.6), lightness=(0.5, 0.7), always_apply=False,p=0.5),
+	    A.CropNonEmptyMaskIfExists (672, 672, ignore_values=[255], ignore_channels=[1], always_apply=True, p=0.8),
+            
+	    # A.RandomCrop(width=700, height=700),
+	    
+	    # A.PadIfNeeded(min_height=512, min_width=512, always_apply=True, border_mode=0),
+            
+        ])
+        
+        image = np.asarray(img)
+        mask = np.asarray(mask)
+        transformed = transform(image=image, mask=mask)
+        transformed_image = transformed['image']
+        transformed_mask = transformed['mask']
+        transformed_image = Image.fromarray(np.uint8(transformed_image))
+        transformed_mask = Image.fromarray(np.uint8(transformed_mask))
+
+        return transformed_image, transformed_mask
+    @staticmethod
+    def predict(pil_img, scale, is_mask):
+        transform = A.Compose([
+            A.PadIfNeeded(min_height=960, min_width=1728, always_apply=True, border_mode=0),
+         ])
+        
+        pil_img = np.asarray(pil_img)
+        img_ndarray = np.asarray(pil_img)
+        if not is_mask:
+            if img_ndarray.ndim == 2:
+                img_ndarray = img_ndarray[np.newaxis, ...]
+            else:
+                img_ndarray = img_ndarray.transpose((2, 0, 1))
+
+            img_ndarray = img_ndarray / 255
+
+        return img_ndarray 
+        
+    
+    
+    def __getitem__(self, idx):
+        name = self.ids[idx]
+        mask_file = list(self.masks_dir.glob(name + self.mask_suffix + '.*'))
+        img_file = list(self.images_dir.glob(name + '.*'))
+
+        assert len(img_file) == 1, f'Either no image or multiple images found for the ID {name}: {img_file}'
+        assert len(mask_file) == 1, f'Either no mask or multiple masks found for the ID {name}: {mask_file}'
+        mask = self.load(mask_file[0])
+        img = self.load(img_file[0])
+
+        assert img.size == mask.size, \
+            f'Image and mask {name} should be the same size, but are {img.size} and {mask.size}'
+            
+        # transform = torch.nn.Sequential(
+            # transforms.RandomRotation(30, expand=False, fill=0, resample=None),
+            # transforms.CenterCrop((512,512)),
+            # transforms.Normalize((0.8263105, 0.6212347, 0.7698729), (0.15582965, 0.268009, 0.1718304)),
+            # transforms.RandomAdjustSharpness(1.2, p=0.5),
+            # transforms.ToTensor(),
+            # )
+        
+        # scripted_transforms = torch.jit.script(transform)
+        img,mask = self.dataAug(img,mask)
+        
+        
+        img = self.preprocess(img, self.scale, is_mask=False)
+        mask = self.preprocess(mask, self.scale, is_mask=True)
+        
+        
+        
+
+        return {
+            'image': torch.as_tensor(img.copy()).float().contiguous(),
+            'mask': torch.as_tensor(mask.copy()).long().contiguous()
+        }
+
+
+
+
+
+class CarvanaDataset(BasicDataset):
+    def __init__(self, images_dir, masks_dir, scale=1):
+        super().__init__(images_dir, masks_dir, scale, mask_suffix='_mask')
